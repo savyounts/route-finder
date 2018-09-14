c52ac3959e8fb5fe56ceac43d376b0360e7d207c
Savannah Younts <savyounts@gmail.com>
2018-09-14 00:17:05 +0000

    create and migrate tables

--- db/migrate/20180913235751_create_routes.rb
+++ db/migrate/20180913235751_create_routes.rb
@@ -2,7 +2,10 @@ class CreateRoutes < ActiveRecord::Migration
   def change
     create_table :routes do |t|
 
-      t.timestamps null: false
+      t.string :name
+      t.string :grade
+      t.string :color, default: "none"
+      t.string :location
     end
   end
 end
--- db/migrate/20180913235801_create_route_statuses.rb
+++ db/migrate/20180913235801_create_route_statuses.rb
@@ -1,8 +1,12 @@
 class CreateRouteStatuses < ActiveRecord::Migration
   def change
     create_table :route_statuses do |t|
-
-      t.timestamps null: false
+      t.integer :user_id
+      t.integer :route_id
+      t.string :status
+      t.text :current_issue
+      t.string :type
+      t.timestamps
     end
   end
 end

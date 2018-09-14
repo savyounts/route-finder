5a6e18f67f2120f108c14d94adfcd351c6a75e4f
Savannah Younts <savyounts@gmail.com>
2018-09-14 00:39:22 +0000

    change RouteStatus.type to climb_style & seed db

--- db/migrate/20180913235801_create_route_statuses.rb
+++ db/migrate/20180913235801_create_route_statuses.rb
@@ -5,7 +5,7 @@ class CreateRouteStatuses < ActiveRecord::Migration
       t.integer :route_id
       t.string :status
       t.text :current_issue
-      t.string :type
+      t.string :climb_style
       t.timestamps
     end
   end
